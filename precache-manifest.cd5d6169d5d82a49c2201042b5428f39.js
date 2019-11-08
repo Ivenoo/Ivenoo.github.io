651da7356928de0379dfdbbcfe6296f9ffc3965d
Ivenoo <32682666+Ivenoo@users.noreply.github.com>
2019-11-08 09:41:57 +0100

    correction

---
+++ precache-manifest.cd5d6169d5d82a49c2201042b5428f39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c358a6767ef13ffcf0585903ed621fa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e550e7bafaecbbfee3f7",
+    "url": "/static/css/main.47bb8139.chunk.css"
+  },
+  {
+    "revision": "d98dc69d1d69a1719196",
+    "url": "/static/js/2.9859bf32.chunk.js"
+  },
+  {
+    "revision": "e550e7bafaecbbfee3f7",
+    "url": "/static/js/main.07e5ab65.chunk.js"
+  },
+  {
+    "revision": "b663f068bb0d4db130e4",
+    "url": "/static/js/runtime-main.546d44e4.js"
+  }
+]);
