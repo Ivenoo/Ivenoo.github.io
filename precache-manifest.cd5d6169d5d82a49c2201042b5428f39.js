a000e5a297cb61f7fad357a9e2e822f603c3bf62
Ivenoo <32682666+Ivenoo@users.noreply.github.com>
2019-11-08 09:33:40 +0100

    repear

--- precache-manifest.cd5d6169d5d82a49c2201042b5428f39.js
+++ precache-manifest.cd5d6169d5d82a49c2201042b5428f39.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d1167b41f0a26d5b06015e80b5fbb8a8",
+    "revision": "c358a6767ef13ffcf0585903ed621fa1",
     "url": "/index.html"
   },
   {
-    "revision": "7aaa46420d22d8c39099",
-    "url": "/static/css/main.75794789.chunk.css"
+    "revision": "e550e7bafaecbbfee3f7",
+    "url": "/static/css/main.47bb8139.chunk.css"
   },
   {
     "revision": "d98dc69d1d69a1719196",
     "url": "/static/js/2.9859bf32.chunk.js"
   },
   {
-    "revision": "7aaa46420d22d8c39099",
-    "url": "/static/js/main.83bb4ef6.chunk.js"
+    "revision": "e550e7bafaecbbfee3f7",
+    "url": "/static/js/main.07e5ab65.chunk.js"
   },
   {
     "revision": "b663f068bb0d4db130e4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a35484876b48e49cd0296161022eb215.js"
+  "/precache-manifest.cd5d6169d5d82a49c2201042b5428f39.js"
 );
 
 self.addEventListener('message', (event) => {
